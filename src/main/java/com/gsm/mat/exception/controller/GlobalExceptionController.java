acf3b976ed69f4ca1bef57a6f4b64f666ec22fc3
jeayoung <k01066624566@gmail.com>
2022-02-28 17:32:54 +0900

    공통되는 부분을 메서드로 묶고 로그를 중복 출력하는 부분을 수정

--- src/main/java/com/gsm/mat/exception/controller/GlobalExceptionController.java
+++ src/main/java/com/gsm/mat/exception/controller/GlobalExceptionController.java
@@ -19,26 +19,28 @@ import javax.servlet.http.HttpServletResponse;
 public class GlobalExceptionController {
     @ExceptionHandler(MemberNotExistsException.class)
     public ResponseEntity<ErrorResponse> MemberNotExistsExceptionHandler(HttpServletRequest request, HttpServletResponse response, MemberNotExistsException ex){
-        log.info(request.getRequestURI());
-        log.error("UserNoExistsException", ex);
-        ex.printStackTrace();
+        printExceptionMessage(request, ex, "UserNoExistsException");
         ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
     }
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> UserNotFoundExceptionHandler(HttpServletRequest request, HttpServletResponse response, UserNotFoundException ex){
-        log.info(request.getRequestURI());
-        log.error("UserNotFoundException", ex);
-        ex.printStackTrace();
+        printExceptionMessage(request, ex, "UserNotFoundException");
         ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
     }
+
     @ExceptionHandler(PasswordNotCorrectException.class)
     public ResponseEntity<ErrorResponse> PasswordNotCorrectExceptionHandler(HttpServletRequest request, HttpServletResponse response, PasswordNotCorrectException ex){
-        log.info(request.getRequestURI());
-        log.error("PasswordNotCorrectException", ex);
-        ex.printStackTrace();
+        printExceptionMessage(request, ex, "PasswordNotCorrectException");
         ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
+    }
+
+    private void printExceptionMessage(HttpServletRequest request, RuntimeException ex, String message) {
+        log.error(request.getRequestURI());
+        log.error(message);
+        ex.printStackTrace();
     }
 }

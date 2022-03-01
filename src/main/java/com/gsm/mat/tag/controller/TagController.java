8d128831e78a1555bc7096d7fe3a4959a83f65e5
jeayoung <k01066624566@gmail.com>
2022-03-02 08:26:27 +0900

    fix createTag할때 List로 받게

--- src/main/java/com/gsm/mat/tag/controller/TagController.java
+++ src/main/java/com/gsm/mat/tag/controller/TagController.java
@@ -9,6 +9,8 @@ import com.gsm.mat.tag.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
@@ -16,8 +18,8 @@ public class TagController {
     private final ResponseService responseService;
     private final TagService tagService;
     @PostMapping("/tag/{notificationIdx}")
-    public CommonResult createTag(@PathVariable Long notificationIdx, @RequestBody TagDto tagDto){
-        tagService.create(tagDto,notificationIdx);
+    public CommonResult createTag(@PathVariable Long notificationIdx, @RequestBody List<TagDto> tagDtoList){
+        tagService.create(tagDtoList,notificationIdx);
         return responseService.getSuccessResult();
     }
     @GetMapping("/tag/{notificationIdx}")
--- src/main/java/com/gsm/mat/tag/service/TagService.java
+++ src/main/java/com/gsm/mat/tag/service/TagService.java
@@ -1,5 +1,6 @@
 package com.gsm.mat.tag.service;
 
+import com.gsm.mat.notification.Notification;
 import com.gsm.mat.notification.service.NotificationService;
 import com.gsm.mat.tag.Tag;
 import com.gsm.mat.tag.dto.TagDto;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,10 +18,15 @@ import java.util.List;
 public class TagService {
     private final TagRepository tagRepository;
     private final NotificationService notificationService;
-    public Long create(TagDto tagDto,Long notificationIdx){
-        Tag tag = tagDto.toEntity();
-        tag.setNotification(notificationService.findOne(notificationIdx));
-        return tagRepository.save(tag);
+    public void create(List<TagDto> tagDto,Long notificationIdx){
+        List<Tag> tags = new ArrayList<>();
+        Notification notification = notificationService.findOne(notificationIdx);
+        for (int i = 0; i < tagDto.size(); i++) {
+            tags.add(tagDto.get(i).toEntity());
+            Tag tag = tags.get(i);
+            tag.setNotification(notification);
+            tagRepository.save(tag);
+        }
     }
     public List<Tag> findByNotification(Long notificationIdx){
         return tagRepository.findByNotification(notificationIdx);

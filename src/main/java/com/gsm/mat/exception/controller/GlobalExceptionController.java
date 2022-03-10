ad74eecfda7b5418a6be3fdbf4720b2520040338
jeayoung <k01066624566@gmail.com>
2022-03-10 09:38:58 +0900

    fix json Not Match exception

--- src/main/java/com/gsm/mat/exception/controller/GlobalExceptionController.java
+++ src/main/java/com/gsm/mat/exception/controller/GlobalExceptionController.java
@@ -46,9 +46,10 @@ public class GlobalExceptionController {
         ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
     }
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> HttpMessageNotReadableExceptionHandler(HttpServletRequest request, HttpServletResponse response, HttpMessageNotReadableException ex){
-        printExceptionMessage(request, ex, "Can't minus notification's goods");
+        printExceptionMessage(request, ex, "Json doesn't match object");
         ErrorResponse errorResponse = new ErrorResponse(ErrorCode.BAD_REQUEST);
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ErrorCode.BAD_REQUEST.getStatus()));
     }

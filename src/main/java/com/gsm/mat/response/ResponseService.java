d6cba80819a27e6ca8c78c96636fa428515691da
jeayoung <k01066624566@gmail.com>
2022-03-11 08:45:17 +0900

    fix ResponseService

--- src/main/java/com/gsm/mat/response/ResponseService.java
+++ src/main/java/com/gsm/mat/response/ResponseService.java
@@ -17,8 +17,7 @@ public class ResponseService {
     public final ObjectMapper mapper;
 
     public enum CommonResponse{
-        SUCCESS(200, "성공하였습니다"),
-        FAIL(-1, "실패하였습니다");
+        SUCCESS(200, "성공하였습니다");
 
         int code;
         String msg;
@@ -57,30 +56,10 @@ public class ResponseService {
         setSuccessResult(result);
         return result;
     }
-    // 실패 결과만 처리하는 메소드
-    public CommonResult getFailResult() {
-        CommonResult result = new CommonResult();
-        result.setSuccess(false);
-        result.setCode(CommonResponse.FAIL.getCode());
-        result.setMsg(CommonResponse.FAIL.getMsg());
-        return result;
-    }
     // 결과 모델에 api 요청 성공 데이터를 세팅해주는 메소드
     private void setSuccessResult(CommonResult result) {
         result.setSuccess(true);
         result.setCode(CommonResponse.SUCCESS.getCode());
         result.setMsg(CommonResponse.SUCCESS.getMsg());
-    }
-    //getFailResult 메소드가 code, msg를 받을수 있도록 수정
-    public CommonResult getFailResult(int code, String msg) {
-        CommonResult result = new CommonResult();
-        result.setSuccess(false);
-        result.setCode(code);
-        result.setMsg(msg);
-        return result;
-    }
-
-    public String getFailResultConvertString(int code, String msg) throws JsonProcessingException {
-        return mapper.writeValueAsString(getFailResult(code, msg));
     }
 }
